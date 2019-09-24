b515795bc029dbd9fc9b24407e73d8ca6331d7c9
peengo <peengo@gmail.com>
2019-09-24 15:12:18 +0200

    update ecosystem config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,10 +1,21 @@
 module.exports = {
     apps: [{
         name: 'server',
-        script: 'npm',
-
-        args: 'start',
-        instances: 4,
+        script: './index.js',
+        instances: 2,
+        autorestart: true,
+        watch: true,
+        max_memory_restart: '100M',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }, {
+        name: 'sync',
+        script: './scripts/sync.js',
+        instances: 1,
         autorestart: true,
         watch: false,
         max_memory_restart: '100M',
